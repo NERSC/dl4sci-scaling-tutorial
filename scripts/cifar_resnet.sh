d33a157b688475c9045e4d6e197d6773150c9a1a
Steve Farrell <sparticlesteve@gmail.com>
2019-07-19 09:34:34 -0700

    Update submit scripts for dl4sci school
    Updated reservation name.
    Added single process dataset downlod to avoid issues.
    Some other minor script submission updates.

--- scripts/cifar_resnet.sh
+++ scripts/cifar_resnet.sh
@@ -3,13 +3,15 @@
 #SBATCH -C knl
 #SBATCH -N 1
 #SBATCH -q regular
-#SBATCH --reservation isc19_dl_tutorial
-#SBATCH -t 30
+#SBATCH --reservation dl4sci
+#SBATCH -t 1:00:00
 #SBATCH -o logs/%x-%j.out
 
+# Load the software
 module load tensorflow/intel-1.13.1-py36
 
-config=configs/cifar10_resnet.yaml
-script=train_horovod.py
+# Ensure dataset is downloaded by single process
+python -c "import keras; keras.datasets.cifar10.load_data()"
 
-srun python $script $config -d
+# Submit multi-node training
+srun -u python train_horovod.py configs/cifar10_resnet.yaml -d
